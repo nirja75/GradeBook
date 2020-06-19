32f272400071b6a7a2099912204726f0db9d3937
Bala <balavignesh_b@live.com>
2020-06-19 08:41:48 -0400

    Update StudentResourse.java

--- src/main/java/com/balavignesh/gradebook/services/StudentResourse.java
+++ src/main/java/com/balavignesh/gradebook/services/StudentResourse.java
@@ -7,8 +7,13 @@ package com.balavignesh.gradebook.services;
 
 import com.balavignesh.gradebook.domain.Student;
 import com.balavignesh.gradebook.domain.StudentList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,20 +31,76 @@ public class StudentResourse {
     
     @POST
     @Path("{name}/grade/{grade}")
-    public Response addCustomer(@PathParam("name") String name,@PathParam("grade") String grade){
-        Student student = new Student();
-        student.setName(name);
-        student.setGrade(grade);
-        studentList.getStudent().add(student);
+    public Response addStudent(@PathParam("name") String name,@PathParam("grade") String grade){
+        return addOrReplaceStudent(name,grade);
+    }
+    
+    @PUT
+    @Path("{name}/grade/{grade}")
+    public Response modifyStudent(@PathParam("name") String name,@PathParam("grade") String grade){
+        return addOrReplaceStudent(name,grade);
+    }
+    
+    private Response addOrReplaceStudent(String name,String grade){
+        
+        if(grade==null || !validGrade(grade)){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        
+        Student studentSaved = filterStudent(studentList,name);
+        
+        if(studentSaved == null ){
+           Student student = new Student();
+           student.setName(name);
+            student.setGrade(grade);
+            studentList.getStudent().add(student);
+        }else{
+            studentSaved.setGrade(grade);
+        }
+        
         return Response.ok().build();
     }
     
-   //GET /customer
-    
     @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public StudentList getCustomers(){
+    @Produces(MediaType.TEXT_XML+";charset=utf-8")
+    public StudentList getStudents(){
         return studentList;
     }
     
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.TEXT_XML+";charset=utf-8")
+    public Student getStudent(@PathParam("name") String name){
+        Student student = filterStudent(studentList,name);
+        if(student==null){
+            throw new NotFoundException();
+        }
+        return student;
+    }
+    
+    @DELETE
+    @Path("{name}")
+    public Response deleteStudent(@PathParam("name") String name){
+        Student student = filterStudent(studentList,name);
+        if(student==null){
+            throw new NotFoundException();
+        }else{
+            studentList.getStudent().remove(student);
+        }
+        return Response.ok().build();
+    }
+
+    private Student filterStudent(StudentList studentList, String name) {
+        if(name == null || name.trim().length()==0 || studentList.getStudent()==null || studentList.getStudent().size()==0){
+            return null;
+        }
+        return studentList.getStudent().stream().filter(student->name.equalsIgnoreCase(student.getName()))
+                .findFirst().orElse(null);
+    }
+
+    private boolean validGrade(String grade) {
+        ArrayList<String> grades = new ArrayList<String>(
+                Arrays.asList("A+","A-","B+","B-","C+","C-","D+","D-","A","B","C","D","E","F","I","W","Z"));
+        return grades.stream().filter(gr->gr.equalsIgnoreCase(grade)).count()==1;
+    }
 }
